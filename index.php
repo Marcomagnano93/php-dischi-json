6812f2fdb6bbb0f7ab93b1cd51c00d201dfef68f
Marcomagnano93 <marcog.magnano@gmail.com>
2024-05-08 15:30:00 +0200

    ADD: style and layout, header

--- index.php
+++ index.php
@@ -10,18 +10,25 @@
 <body>
 <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <div id="app">
-    <main class="container">
-        <div class="row gap-3">
-        <div class="card p-3" style="width: 18rem;"
-        v-for="disc in discs"
-        >
-            <img :src="disc.poster" class="card-img-top" alt="disc poster image">
-            <div class="card-body">
-                <h5 class="card-title">{{ disc.title }}</h5>
-                <p class="card-text">Autore: {{ disc.author }}</p>
-                <p class="card-text">Anno di rilascio: {{ disc.year }}</p>
-                <p class="card-text">Genere: {{ disc.genre }}</p>
-            </div>
+    <header>
+        <div class="container-fluid text-center bg-primary py-3">
+            <h1>Catalogo CD</h1>
+        </div>
+    </header>
+    <main class="container-fluid py-5 bg-danger-subtle">
+        <div class="container">
+            <div class="row gap-3">
+                <div class="card p-3 bg-info-subtle" style="width: 18rem;"
+                v-for="disc in discs"
+                >
+                    <img :src="disc.poster" class="card-img-top" alt="disc poster image">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ disc.title }}</h5>
+                        <p class="card-text">Autore: {{ disc.author }}</p>
+                        <p class="card-text">Anno di rilascio: {{ disc.year }}</p>
+                        <p class="card-text">Genere: {{ disc.genre }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
